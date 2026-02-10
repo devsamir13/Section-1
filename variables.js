ad3bd8b6df94e64269821c51eb7b1b3e39f82794
devsamir13 <samir5827245@gmail.com>
2026-02-10 22:28:45 +0530

    Updated

---
+++ variables.js
@@ -0,0 +1,14 @@
+a = 10;
+console.log(a);
+
+console.log('Hello, Javascript');
+console.log(typeof a);
+
+userName ='Samir'
+console.log(userName);
+
+isHungry = true;
+console.log(isHungry);
+
+kuchnhi = null;
+console.log(kuchnhi);
